994caceb45591d78edd9bb2006d79881c8e31924
lucia <ruadaslucia@gmail.com>
2022-05-13 08:44:56 +0200

    my changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const allMovies = require('./assets/allMovies.json');
 
 // create and config server
 const server = express();
@@ -10,4 +11,21 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, res) => {
+  const genderFilterParams = req.query['gender'];
+  let filteredMovies = [...allMovies];
+
+  if (genderFilterParams !== '') {
+    filteredMovies = allMovies.filter(
+      (movie) => movie.gender === genderFilterParams
+    );
+  }
+
+  const response = {
+    success: true,
+    movies: filteredMovies,
+  };
+  res.json(response);
 });

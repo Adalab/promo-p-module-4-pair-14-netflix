8cd5643c2e228b9137ad6b0a8bedeb9e50b0bda3
lucia <ruadaslucia@gmail.com>
2022-05-19 18:07:39 +0200

    users login not finish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const allMovies = require('./assets/allMovies.json');
+const users = require('./data/users.json');
 
 // create and config server
 const server = express();
@@ -13,7 +14,9 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+//--------endpoints
 
+//filter movies endpoint
 server.get('/movies', (req, res) => {
   const genderFilterParams = req.query['gender'];
   let filteredMovies = [...allMovies];
@@ -31,9 +34,33 @@ server.get('/movies', (req, res) => {
   res.json(response);
 });
 
+//get movies id endpoint
 server.get('/movie/:movieId', (req, res) => {
   const foundMovie = allMovies.find((movie) => movie.id === req.params.movieId);
   res.render('movie', foundMovie);
+});
+
+//users endpoint
+server.post('/login', (req, res) => {
+  let exist = users.find((user) => {
+    if (user.email === req.body.email && user.password === req.body.password) {
+      return user;
+    }
+    console.log(user.email);
+    console.log(user.password);
+    return null;
+  });
+
+  if (!exist) {
+    return res.status(404).json({
+      success: false,
+      errorMessage: 'Usuaria/o no encontrada/o',
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    userId: exist.id,
+  });
 });
 
 //static server

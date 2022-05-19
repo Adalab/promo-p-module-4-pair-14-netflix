f3650334674f7de6878b1b6cbfb5dd2cc794c894
lucia <ruadaslucia@gmail.com>
2022-05-19 11:27:58 +0200

    fixed css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const allMovies = require("./assets/allMovies.json");
+const express = require('express');
+const cors = require('cors');
+const allMovies = require('./assets/allMovies.json');
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
-server.set("view engine", "ejs");
+server.set('view engine', 'ejs');
 
 // init express aplication
 const serverPort = 4000;
@@ -14,11 +14,11 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get("/movies", (req, res) => {
-  const genderFilterParams = req.query["gender"];
+server.get('/movies', (req, res) => {
+  const genderFilterParams = req.query['gender'];
   let filteredMovies = [...allMovies];
 
-  if (genderFilterParams !== "") {
+  if (genderFilterParams !== '') {
     filteredMovies = allMovies.filter(
       (movie) => movie.gender === genderFilterParams
     );
@@ -31,18 +31,18 @@ server.get("/movies", (req, res) => {
   res.json(response);
 });
 
-server.get("/movie/:movieId", (req, res) => {
+server.get('/movie/:movieId', (req, res) => {
   const foundMovie = allMovies.find((movie) => movie.id === req.params.movieId);
-  res.render("movie", foundMovie);
+  res.render('movie', foundMovie);
 });
 
 //static server
 
-const staticServerPathAdmin = "./src/public-react";
+const staticServerPathAdmin = './src/public-react';
 server.use(express.static(staticServerPathAdmin));
 
-const staticServerPathAdmin2 = "./src/public-movies-images";
+const staticServerPathAdmin2 = './src/public-movies-images';
 server.use(express.static(staticServerPathAdmin2));
 
-const staticServerPathAdminStyles = "./src/public-styles";
+const staticServerPathAdminStyles = './src/publicStyles';
 server.use(express.static(staticServerPathAdminStyles));
